25bdabad00efc3f759c19e2a8fc0eaa7d1cc72ed
Mujtaba Mateen <85826852+Mujtaba523@users.noreply.github.com>
2023-06-17 12:28:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,6 @@ else:
             yaxis_title="Importance",
             font=dict(color="#ffc930", size=20),
             height=600,
-            width=1400
         )
         fig.update_xaxes(showgrid=True, gridcolor="white")
         fig.update_yaxes(showgrid=True, gridcolor="white")
