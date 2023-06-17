5c783c91fae0076ede8c3a5d9e7732df79139634
Mujtaba Mateen <85826852+Mujtaba523@users.noreply.github.com>
2023-06-17 12:51:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ else:
             yaxis_title="Feature",     # Update axis titles
             font=dict(color="#ffc930", size=20),
             height=600,
-            width=1400
+            width=400
         )
         fig.update_xaxes(showgrid=True, gridcolor="white")
         fig.update_yaxes(showgrid=True, gridcolor="white")
